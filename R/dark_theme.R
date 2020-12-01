8fcb1901429cacada11d1d4a7ded08d7827ef6a8
cbreenmachine <coleman.breen@wisc.edu>
2020-12-01 14:52:41 -0500

    First two functions of first R package!

---
+++ R/dark_theme.R
@@ -0,0 +1,6 @@
+#' Adds a dark theme to a ggplot object with sensible font sizes
+#' and styles
+
+dark_theme <- function(){
+  
+}
---
+++ R/print_idioms.R
@@ -0,0 +1,7 @@
+#' Prints my favorite coding idioms to the console
+
+print_idioms <- function(){
+  
+  
+  
+}
