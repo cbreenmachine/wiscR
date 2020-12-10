4c1c8e69697bb3414619175c60cbadfc0147ad3d
cbreenmachine <coleman.breen@wisc.edu>
2020-12-10 15:13:35 -0500

    fixed export issue

--- R/plot_DFFITS.R
+++ R/plot_DFFITS.R
@@ -1,24 +1,25 @@
+#' @export plot_DFFITS
 #'plot_DFBETAS
 #'DFBETAS to come
 
 plot_DFFITS <- function(model){
-  
+
   # How many observations (n) and predictors (p)
   p <- length(model$coefficients)
   n <- model$df.residual + p
-  
+
   # Here, 100 is considered the cutoff for a "large" dataset
   cutoff <- ifelse(n > 100, 1, 2 * sqrt(p / n))
-  
+
   # DFFITS
   DFFITS <- dffits(model)
-  
+
   df <- data.frame(index = 1:length(DFFITS), DFFITS = DFFITS)
-  
+
   # Which points are high leverage?
   DFFITS.df <- df %>%
     filter(DFFITS > cutoff)
-  
+
   # Create plot
   p <-  df %>%
     ggplot(aes(x = index, y = DFFITS)) +
@@ -26,11 +27,11 @@ plot_DFFITS <- function(model){
     ggtitle("DFFITS above or below cutoff be problematic") +
     geom_hline(yintercept = cutoff, color = "red") +
     geom_hline(yintercept = -cutoff, color = "red")
-  
+
   output <- list()
-  
+
   output$plot <- p
   output$df <- DFFITS.df
-  
+
   return(output)
 }
--- R/qqplot.R
+++ R/qqplot.R
@@ -1,12 +1,13 @@
+#' @export qqplot
 #'qqplot
-#'Returns a ggplot object that can be costomized using grammar of graphics 
+#'Returns a ggplot object that can be costomized using grammar of graphics
 #'instead of R's janky base graphics
 
 qqplot <- function(x){
-  
+
 data.frame(x = model$residuals) %>%
   ggplot(aes(sample = x)) +
-  stat_qq() + 
+  stat_qq() +
   stat_qq_line(color = "red") %>%
   return()
 }
