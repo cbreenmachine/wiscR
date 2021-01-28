eb0354131535341aac07add16cd4aabdd7244370
cbreenmachine <coleman.breen@wisc.edu>
2021-01-28 10:39:10 -0500

    bones of light theme

---
+++ R/light_theme.R
@@ -0,0 +1,13 @@
+#' Adds a light theme to a ggplot object with sensible font sizes
+#' and styles
+#' @export
+dark_theme <- function(){
+  require(tidyverse)
+
+  theme_minimal() %+replace%
+    theme(text = element_text(size = rel(4.5)),
+          panel.grid.major = element_line(size = 0.2),
+          panel.grid.minor = element_line(size = 0.5),
+          strip.background = element_rect(size = 0),
+          plot.background = element_rect(size = 2)
+}
